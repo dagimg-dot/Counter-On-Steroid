ca985b5da091b26e4dc6f7b013f37ec7e771f6bc
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-08 21:09:01 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/counter_on_steroid/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.counter_on_steroid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:counter_on_steroid/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter on Steroid',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
