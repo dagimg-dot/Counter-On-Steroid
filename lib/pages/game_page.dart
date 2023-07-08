e6dc754d489cca3cef3d7705cf89d3736c99129b
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-09 00:18:01 +0300

    fully implement the game page

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_app_bar.dart';
+import 'home_page.dart';
+
 class GamePage extends StatefulWidget {
-  const GamePage({super.key, required this.counted});
+  const GamePage({Key? key, required this.counted}) : super(key: key);
 
   final int counted;
 
@@ -10,14 +13,198 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
+  final Color mainColor = const Color(0xFF121212);
+  int _currentNumber = 2;
+  int score = 0;
+  List<int> scores = [];
+
+  bool isCorrect(int currNum) {
+    if (widget.counted % currNum == 0) {
+      return true;
+    }
+    return false;
+  }
+
+  int calCounted(int num) {
+    return num - 2;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Widget'),
+      backgroundColor: mainColor,
+      appBar: CustomAppBar(
+        mainColor: mainColor,
+        action: IconButton(
+          onPressed: () {
+            showCloseDialog(context);
+          },
+          icon: const Icon(Icons.home_filled),
+        ),
       ),
-      body: Center(
-        child: Text('${widget.counted}'),
-      ),);
+      body: Container(
+        margin: const EdgeInsets.only(top: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              'Is $_currentNumber a factor of ${widget.counted}?',
+              style: const TextStyle(fontSize: 24, color: Colors.white),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    if (widget.counted != _currentNumber) {
+                      bool isFactor = isCorrect(_currentNumber);
+                      if (isFactor) {
+                        score += 1;
+                      }
+                      setState(() {
+                        _currentNumber += 1;
+                      });
+                    } else {
+                      showGameOverDialog(context);
+                    }
+                  },
+                  child: const Text('Yes'),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (widget.counted != _currentNumber) {
+                      bool isFactor = isCorrect(_currentNumber);
+                      if (!isFactor) {
+                        score += 1;
+                      }
+                      setState(() {
+                        _currentNumber += 1;
+                      });
+                    } else {
+                      showGameOverDialog(context);
+                    }
+                  },
+                  child: const Text('No'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Your Scores',
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Expanded(
+                child: ListView.builder(
+              itemCount: scores.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  tileColor: Colors.grey[900],
+                  title: Text(
+                    '${scores[index]} / ${calCounted(widget.counted)}',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  trailing: Icon(
+                    scores[index] > widget.counted / 2
+                        ? Icons.check
+                        : Icons.close,
+                    color: Colors.white,
+                  ),
+                );
+              },
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<dynamic> showGameOverDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+          title: const Text('Game Over!'),
+          content: Text(
+            'Your score is $score / ${calCounted(widget.counted)}. \n\nDo you want to play again?',
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                scores.add(score);
+                setState(() {
+                  _currentNumber = 2;
+                  score = 0;
+                });
+                Navigator.of(context).pop();
+              },
+              child: const Text('Yes'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const MyHomePage(),
+                  ),
+                );
+              },
+              child: const Text('No'),
+            ),
+          ]),
+    );
+  }
+
+  Future<dynamic> showCloseDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          title: const Text('Are you sure?'),
+          content: const Text(
+            'You will lose all your progress!',
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const MyHomePage(),
+                  ),
+                );
+              },
+              child: Text(
+                'Yes',
+                style: TextStyle(
+                  color: mainColor,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'No',
+                style: TextStyle(
+                  color: mainColor,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
